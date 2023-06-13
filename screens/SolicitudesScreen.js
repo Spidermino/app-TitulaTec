7e44adc67885e4820175254718257f1360cce411
Israel de Jesús Gil Martínez <igil310@accitesz.com>
2023-06-13 02:08:11 -0600

    ---

--- screens/SolicitudesScreen.js
+++ screens/SolicitudesScreen.js
@@ -1,34 +1,26 @@
 import React, {useState} from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList, TouchableOpacity} from "react-native";
 import { ListItem } from 'react-native-elements';
 
 export default function SolicitudesScreen() {
-  const [solicitud, setSolicitud] = useState();
   const [proyecto, setProyecto] = useState();
-
-  const getSolicitudData = async () => {
+  const filter = {
+    where: {
+      idProyecto: '2',
+    },
+  };
+  const getProyectoData = async () => {
     try {
       //   const headers = { "Content-Type": "application/json" };
-      let response = await fetch("http://192.168.1.148:3000/solicitude");
+      let response = await fetch("http://192.168.0.176:3000/proyectos?filter=" + JSON.stringify(filter));
       let data = await response.json();
-      setSolicitud(data);
+      setProyecto(data);
     } catch (error) {
       console.error(error);
     }
   };
-  const getProyectoData = async () => {
-    try{
-      let response = await fetch("http://192.168.1.148:3000/proyecto")
-      let data = await response.json();
-      setProyecto(data);
-    }catch(error){
-      console.error(error);
-    }
-    };
-
-
   useState(() => {
-    getSolicitudData();
+    getProyectoData();
   }, []);
 
   const renderItem = ({ item }) => {
@@ -41,15 +33,43 @@ export default function SolicitudesScreen() {
         }
       >
         <View style={styles.item}>
-          <Text style={styles.title}>{item.nomAlumno}</Text>
-          <Text>Edad: {item.edad}</Text>
+          <Text style={styles.title}>{item.nombre}</Text>
+          <Text>No control: {item.noControl}</Text>
         </View>
       </TouchableOpacity>
     );
   };
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-     
+      <FlatList
+        data={proyecto}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.idProyecto}
+      />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  item: {
+    width: 400,
+    padding: 5,
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderBottomWidth: 1,
+    borderBottomColor: "#30A2FF",
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 12,
+    color: "#ccc",
+  },
+});
